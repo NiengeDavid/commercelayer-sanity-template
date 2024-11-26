0afedf8a897fde04539af0824f9f9fb37aa5ab4e
Nienge David <97910242+NiengeDavid@users.noreply.github.com>
2024-11-26 10:21:32 +0100

    Initial Setup

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,7 +23,7 @@ const Header: React.FC<Props> = ({ lang, countryCode, pageTitle }) => {
           <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
             <div className="flex items-center justify-between w-full md:w-auto">
               <Link href={`/${countryCode}/${lang}`} passHref>
-                <span className="sr-only">Commerce Layer</span>
+                <span className="sr-only">D & S Apparels</span>
                 <Image
                   className="h-8 w-auto"
                   src="//data.commercelayer.app/assets/logos/full-logo/black/commercelayer_full_logo_black.svg"
